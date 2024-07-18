e98c1923a88a7c2d7b16b5d15571d27e9037fb3f
unknown <tgeneste@alliance-digital.fr>
2024-07-18 10:15:40 +0200

    Merge

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const clientRoutes = require('./Routes/ClientRoutes');
 const categorieRoutes = require('./Routes/CategorieRoutes');
+const cors = require('cors');
 const app = express();
 const port = 3001;
 // Précise que notre API fonctionne avec des fichiers JSON
 app.use(express.json());
 // 5 Types de requêtes possible => GET, POST, PATCH, DELETE, PUT 
-
+app.use(cors());
 // Premier parametre est la route pour acceder aux données
 // Deuxieme parametre est la function fléché qui va faire le traitement, prend toujours deux parametre (request, result)
 app.get('/hello', (request, result) => {
